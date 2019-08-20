9e040100c730068c67601c35b24dfb839f893596
Frain <frainl@outlook.com>
2019-08-20 20:26:13 +0800

    chore: add simply view controller for demo

--- SundayListKit.playground/Pages/Demo.xcplaygroundpage/Contents.swift
+++ SundayListKit.playground/Pages/Demo.xcplaygroundpage/Contents.swift
@@ -1,39 +1,51 @@
 import SundayListKit
-//
-//class ExampleCell: UICollectionViewCell {
-//    var text = ""
-//}
-//
-//class ExampleListAdapter: CollectionListAdapter {
-//
-//}
-//
-//class OtherClass: ListModelSource {
-//    func model<List>(for context: ListIndexContext<List, Self>) -> Int where List : ListView {
-//        fatalError()
-//    }
-//
-//    typealias Model = Int
-//
-//
-//}s
+import PlaygroundSupport
+import UIKit
+
+let models = ["Roy", "Pinlin", "Zhiyi", "Frain", "Jack", "Cookie", "Kubrick", "Jeremy"]
+func randomModels() -> [String] {
+    var shuffledModels = models.shuffled()
+    shuffledModels.removeFirst()
+    shuffledModels.removeLast()
+    return shuffledModels.shuffled()
+}
+
+class ViewController: UIViewController, TableListAdapter {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tableView = UITableView(frame: view.bounds)
+        view.addSubview(tableView)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .refresh,
+            target: self,
+            action: #selector(refresh)
+        )
+        
+        tableView.coordinator = tableCoordinator()
+    }
+    
+    @objc func refresh() {
+        performUpdate()
+    }
+}
+
+extension ViewController: TableListAdapter {
+    typealias Item = String
+    var source: [String] {
+        return randomModels()
+    }
+    
+    func tableContext(_ context: TableListContext, cellForItem item: String) -> UITableViewCell {
+        return context.dequeueReusableCell(withCellClass: UITableViewCell.self) {
+            $0.textLabel?.text = item
+        }
+    }
+}
 
 
-//import CoreData
-//
-//class Fetch: NSObject, NSFetchRequestResult { }
-//
-//class TestExample: FetchedResultControllerDataSource, CollectionListAdapter {
-//    func cellForViewModel<List>(for context: ListIndexContext<List, Self>, viewModel: Fetch) -> List.Cell where List : ListView {
-//        fatalError()
-//    }
-//
-//    typealias Model = Fetch
-//
-//    var fetchedResultController = NSFetchedResultsController<TestExample.Model>()
-//
-//
-//
-//}
-
-
+let viewController = ViewController()
+let navigationController = UINavigationController(rootViewController: viewController)
+navigationController.view.frame = CGRect(x: 0, y: 0, width: 320, height: 600)
+PlaygroundPage.current.needsIndefiniteExecution = true
+PlaygroundPage.current.liveView = navigationController.view

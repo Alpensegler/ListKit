e1a790d895bd5de2fc48937e985c47664ac2660b
Frain <frainl@outlook.com>
2019-10-10 20:01:31 +0800

    refactor: add base code

---
+++ Sources/DataSource/Snapshot.swift
@@ -0,0 +1,10 @@
+//
+//  Snapshot.swift
+//  ListKit
+//
+//  Created by Frain on 2019/10/10.
+//
+
+struct Snapshot<Item> {
+    
+}
---
+++ Sources/DataSource/Updater.swift
@@ -0,0 +1,10 @@
+//
+//  Updater.swift
+//  ListKit
+//
+//  Created by Frain on 2019/10/10.
+//
+
+struct Updater<Source, Item> {
+    
+}

e1af1b454fbfb4dcf685140014e58e2d8ec4ac68
Frain <frainl@outlook.com>
2019-12-14 04:25:42 +0800

    refactor

--- Sources/ListAdapter/CollectionListAdapter.swift
+++ Sources/ListAdapter/CollectionListAdapter.swift
@@ -23,7 +23,7 @@ where Source.SourceBase == Source {
     public var sourceBase: Source { source }
     public var collectionList: CollectionList<Source> { self }
     public func makeListCoordinator() -> ListCoordinator<Source> {
-        addToStorage(source.makeListCoordinator())
+        addToStorage(source.listCoordinator)
     }
     
     public var wrappedValue: Source { source }
--- Sources/Source/AnySource.swift
+++ Sources/Source/AnySource.swift
@@ -10,20 +10,19 @@ public protocol AnySourceConvertible: DataSource {
 }
 
 public struct AnySource<Item>: UpdatableDataSource, AnySourceConvertible {
-    var coordinatorMaker: () -> ListCoordinator<AnySource<Item>>
+    var coordinatorMaker: (Self) -> ListCoordinator<AnySource<Item>>
     
     public let source: Any
     public var updater: Updater<Self>
-    public var coordinatorStorage = CoordinatorStorage<AnySource<Item>>()
-    public func makeListCoordinator() -> ListCoordinator<AnySource<Item>> { coordinatorMaker() }
+    public let coordinatorStorage = CoordinatorStorage<AnySource<Item>>()
+    public func makeListCoordinator() -> ListCoordinator<AnySource<Item>> { coordinatorMaker(self) }
     
     public init<Source: DataSource>(_ dataSource: Source) where Source.SourceBase.Item == Item {
         let updater = dataSource.updater
         let differ = Differ<Self>(differ: updater.source) { (($0.source) as? Source)?.sourceBase }
         self.source = dataSource
         self.updater = Updater(source: differ, item: updater.item)
-        coordinatorMaker = { AnyItemSourceCoordinator(dataSource, coordinator: dataSource.listCoordinator) }
-        coordinatorStorage.coordinator = AnyItemSourceCoordinator(dataSource, coordinator: dataSource.listCoordinator)
+        self.coordinatorMaker = { $0.addToStorage(AnyItemSourceCoordinator(dataSource)) }
     }
 }
 

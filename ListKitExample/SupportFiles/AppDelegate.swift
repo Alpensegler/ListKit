96f3807ac25311cb16bb3e56cc7c4b7b7ceb0f61
Frain <frainl@outlook.com>
2020-04-23 19:49:50 +0800

    refactor

---
+++ ListKitExample/SupportFiles/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  ListKitExample
+//
+//  Created by Frain on 2020/4/23.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+

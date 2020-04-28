da85f8a88c3902881d65d13306137a5a61dfd44c
Frain <frainl@outlook.com>
2020-04-28 16:54:33 +0800

    refactor

--- Sources/ListAdapter/ListAdapter+FunctionBuilder/AnyCollectionSourcesBuilder.swift
+++ Sources/ListAdapter/ListAdapter+FunctionBuilder/AnyCollectionSourcesBuilder.swift
@@ -9,74 +9,69 @@ public typealias AnyCollectionSources = CollectionList<AnySources>
 
 public extension CollectionList where SourceBase == AnySources {
     init<Source: CollectionListAdapter>(_ dataSource: Source) {
-        self.init(SourceBase(dataSource.collectionList))
+        self.init(AnySources(dataSource.collectionList))
     }
     
-    init<Source: CollectionListAdapter>(_ dataSources: [Source]) {
-        self.init(SourceBase(dataSources))
-    }
-    
-    init(@AnyCollectionSourcesBuilder content: () -> Self) {
-        let dataSource = content()
-        self.init(coordinatorSetups: dataSource.coordinatorSetups, source: dataSource.sourceBase)
+    init<Source: CollectionListAdapter>(@AnyCollectionSourcesBuilder content: () -> Source) {
+        self.init(content())
     }
 }
 
 @_functionBuilder
 public struct AnyCollectionSourcesBuilder {
-    public static func buildIf<S: CollectionListAdapter>(_ content: S?) -> AnyCollectionSources {
-        AnyCollectionSources(content.map { [AnyCollectionSources($0)] } ?? [])
+    public static func buildIf<S: CollectionListAdapter>(_ content: S?) -> S? {
+        content
     }
 
-    public static func buildEither<TrueContent: CollectionListAdapter>(first: TrueContent) -> AnyCollectionSources {
-        AnyCollectionSources([first])
+    public static func buildEither<TrueContent: CollectionListAdapter>(first: TrueContent) -> TrueContent {
+        first
     }
 
-    public static func buildEither<FalseContent: CollectionListAdapter>(second: FalseContent) -> AnyCollectionSources {
-        AnyCollectionSources([second])
+    public static func buildEither<FalseContent: CollectionListAdapter>(second: FalseContent) -> FalseContent {
+        second
     }
     
-    public static func buildBlock() -> AnyCollectionSources {
-        AnyCollectionSources([AnyCollectionSources]())
+    public static func buildBlock() -> CollectionList<Sources<[AnyCollectionSources], Any>> {
+        CollectionList(Sources(dataSources: []))
     }
 
-    public static func buildBlock<S: CollectionListAdapter>(_ content: S) -> AnyCollectionSources {
-        AnyCollectionSources([AnyCollectionSources(content)])
+    public static func buildBlock<S: CollectionListAdapter>(_ content: S) -> S {
+        content
     }
     
-    public static func buildBlock<S0: CollectionListAdapter, S1: CollectionListAdapter>(_ s0: S0, _ s1: S1) -> AnyCollectionSources {
-        AnyCollectionSources([AnyCollectionSources(s0), AnyCollectionSources(s1)])
+    public static func buildBlock<S0: CollectionListAdapter, S1: CollectionListAdapter>(_ s0: S0, _ s1: S1) -> CollectionList<Sources<[AnyCollectionSources], Any>> {
+        CollectionList(Sources(dataSources: [AnyCollectionSources(s0), AnyCollectionSources(s1)]))
     }
     
-    public static func buildBlock<S0: CollectionListAdapter, S1: CollectionListAdapter, S2: CollectionListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2) -> AnyCollectionSources {
-        AnyCollectionSources([AnyCollectionSources(s0), AnyCollectionSources(s1), AnyCollectionSources(s2)])
+    public static func buildBlock<S0: CollectionListAdapter, S1: CollectionListAdapter, S2: CollectionListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2) -> CollectionList<Sources<[AnyCollectionSources], Any>> {
+        CollectionList(Sources(dataSources: [AnyCollectionSources(s0), AnyCollectionSources(s1), AnyCollectionSources(s2)]))
     }
     
-    public static func buildBlock<S0: CollectionListAdapter, S1: CollectionListAdapter, S2: CollectionListAdapter, S3: CollectionListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3) -> AnyCollectionSources {
-        AnyCollectionSources([AnyCollectionSources(s0), AnyCollectionSources(s1), AnyCollectionSources(s2), AnyCollectionSources(s3)])
+    public static func buildBlock<S0: CollectionListAdapter, S1: CollectionListAdapter, S2: CollectionListAdapter, S3: CollectionListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3) -> CollectionList<Sources<[AnyCollectionSources], Any>> {
+        CollectionList(Sources(dataSources: [AnyCollectionSources(s0), AnyCollectionSources(s1), AnyCollectionSources(s2), AnyCollectionSources(s3)]))
     }
     
-    public static func buildBlock<S0: CollectionListAdapter, S1: CollectionListAdapter, S2: CollectionListAdapter, S3: CollectionListAdapter, S4: CollectionListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4) -> AnyCollectionSources {
-        AnyCollectionSources([AnyCollectionSources(s0), AnyCollectionSources(s1), AnyCollectionSources(s2), AnyCollectionSources(s3), AnyCollectionSources(s4)])
+    public static func buildBlock<S0: CollectionListAdapter, S1: CollectionListAdapter, S2: CollectionListAdapter, S3: CollectionListAdapter, S4: CollectionListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4) -> CollectionList<Sources<[AnyCollectionSources], Any>> {
+        CollectionList(Sources(dataSources: [AnyCollectionSources(s0), AnyCollectionSources(s1), AnyCollectionSources(s2), AnyCollectionSources(s3), AnyCollectionSources(s4)]))
     }
     
-    public static func buildBlock<S0: CollectionListAdapter, S1: CollectionListAdapter, S2: CollectionListAdapter, S3: CollectionListAdapter, S4: CollectionListAdapter, S5: CollectionListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5) -> AnyCollectionSources {
-        AnyCollectionSources([AnyCollectionSources(s0), AnyCollectionSources(s1), AnyCollectionSources(s2), AnyCollectionSources(s3), AnyCollectionSources(s4), AnyCollectionSources(s5)])
+    public static func buildBlock<S0: CollectionListAdapter, S1: CollectionListAdapter, S2: CollectionListAdapter, S3: CollectionListAdapter, S4: CollectionListAdapter, S5: CollectionListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5) -> CollectionList<Sources<[AnyCollectionSources], Any>> {
+        CollectionList(Sources(dataSources: [AnyCollectionSources(s0), AnyCollectionSources(s1), AnyCollectionSources(s2), AnyCollectionSources(s3), AnyCollectionSources(s4), AnyCollectionSources(s5)]))
     }
     
-    public static func buildBlock<S0: CollectionListAdapter, S1: CollectionListAdapter, S2: CollectionListAdapter, S3: CollectionListAdapter, S4: CollectionListAdapter, S5: CollectionListAdapter, S6: CollectionListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6) -> AnyCollectionSources {
-        AnyCollectionSources([AnyCollectionSources(s0), AnyCollectionSources(s1), AnyCollectionSources(s2), AnyCollectionSources(s3), AnyCollectionSources(s4), AnyCollectionSources(s5), AnyCollectionSources(s6)])
+    public static func buildBlock<S0: CollectionListAdapter, S1: CollectionListAdapter, S2: CollectionListAdapter, S3: CollectionListAdapter, S4: CollectionListAdapter, S5: CollectionListAdapter, S6: CollectionListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6) -> CollectionList<Sources<[AnyCollectionSources], Any>> {
+        CollectionList(Sources(dataSources: [AnyCollectionSources(s0), AnyCollectionSources(s1), AnyCollectionSources(s2), AnyCollectionSources(s3), AnyCollectionSources(s4), AnyCollectionSources(s5), AnyCollectionSources(s6)]))
     }
     
-    public static func buildBlock<S0: CollectionListAdapter, S1: CollectionListAdapter, S2: CollectionListAdapter, S3: CollectionListAdapter, S4: CollectionListAdapter, S5: CollectionListAdapter, S6: CollectionListAdapter, S7: CollectionListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6, _ s7: S7) -> AnyCollectionSources {
-        AnyCollectionSources([AnyCollectionSources(s0), AnyCollectionSources(s1), AnyCollectionSources(s2), AnyCollectionSources(s3), AnyCollectionSources(s4), AnyCollectionSources(s5), AnyCollectionSources(s6), AnyCollectionSources(s7)])
+    public static func buildBlock<S0: CollectionListAdapter, S1: CollectionListAdapter, S2: CollectionListAdapter, S3: CollectionListAdapter, S4: CollectionListAdapter, S5: CollectionListAdapter, S6: CollectionListAdapter, S7: CollectionListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6, _ s7: S7) -> CollectionList<Sources<[AnyCollectionSources], Any>> {
+        CollectionList(Sources(dataSources: [AnyCollectionSources(s0), AnyCollectionSources(s1), AnyCollectionSources(s2), AnyCollectionSources(s3), AnyCollectionSources(s4), AnyCollectionSources(s5), AnyCollectionSources(s6), AnyCollectionSources(s7)]))
     }
     
-    public static func buildBlock<S0: CollectionListAdapter, S1: CollectionListAdapter, S2: CollectionListAdapter, S3: CollectionListAdapter, S4: CollectionListAdapter, S5: CollectionListAdapter, S6: CollectionListAdapter, S7: CollectionListAdapter, S8: CollectionListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6, _ s7: S7, _ s8: S8) -> AnyCollectionSources {
-        AnyCollectionSources([AnyCollectionSources(s0), AnyCollectionSources(s1), AnyCollectionSources(s2), AnyCollectionSources(s3), AnyCollectionSources(s4), AnyCollectionSources(s5), AnyCollectionSources(s6), AnyCollectionSources(s7)])
+    public static func buildBlock<S0: CollectionListAdapter, S1: CollectionListAdapter, S2: CollectionListAdapter, S3: CollectionListAdapter, S4: CollectionListAdapter, S5: CollectionListAdapter, S6: CollectionListAdapter, S7: CollectionListAdapter, S8: CollectionListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6, _ s7: S7, _ s8: S8) -> CollectionList<Sources<[AnyCollectionSources], Any>> {
+        CollectionList(Sources(dataSources: [AnyCollectionSources(s0), AnyCollectionSources(s1), AnyCollectionSources(s2), AnyCollectionSources(s3), AnyCollectionSources(s4), AnyCollectionSources(s5), AnyCollectionSources(s6), AnyCollectionSources(s7), AnyCollectionSources(s8)]))
     }
     
-    public static func buildBlock<S0: CollectionListAdapter, S1: CollectionListAdapter, S2: CollectionListAdapter, S3: CollectionListAdapter, S4: CollectionListAdapter, S5: CollectionListAdapter, S6: CollectionListAdapter, S7: CollectionListAdapter, S8: CollectionListAdapter, S9: CollectionListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6, _ s7: S7, _ s8: S8, _ s9: S9) -> AnyCollectionSources {
-        AnyCollectionSources([AnyCollectionSources(s8), AnyCollectionSources(s0), AnyCollectionSources(s1), AnyCollectionSources(s2), AnyCollectionSources(s3), AnyCollectionSources(s4), AnyCollectionSources(s5), AnyCollectionSources(s6), AnyCollectionSources(s7), AnyCollectionSources(s8), AnyCollectionSources(s9)])
+    public static func buildBlock<S0: CollectionListAdapter, S1: CollectionListAdapter, S2: CollectionListAdapter, S3: CollectionListAdapter, S4: CollectionListAdapter, S5: CollectionListAdapter, S6: CollectionListAdapter, S7: CollectionListAdapter, S8: CollectionListAdapter, S9: CollectionListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6, _ s7: S7, _ s8: S8, _ s9: S9) -> CollectionList<Sources<[AnyCollectionSources], Any>> {
+        CollectionList(Sources(dataSources: [AnyCollectionSources(s0), AnyCollectionSources(s1), AnyCollectionSources(s2), AnyCollectionSources(s3), AnyCollectionSources(s4), AnyCollectionSources(s5), AnyCollectionSources(s6), AnyCollectionSources(s7), AnyCollectionSources(s8), AnyCollectionSources(s9)]))
     }
 }
--- Sources/ListAdapter/ListAdapter+FunctionBuilder/AnyTableSourcesBuilder.swift
+++ Sources/ListAdapter/ListAdapter+FunctionBuilder/AnyTableSourcesBuilder.swift
@@ -9,74 +9,69 @@ public typealias AnyTableSources = TableList<AnySources>
 
 public extension TableList where SourceBase == AnySources {
     init<Source: TableListAdapter>(_ dataSource: Source) {
-        self.init(SourceBase(dataSource.tableList))
+        self.init(AnySources(dataSource.tableList))
     }
     
-    init<Source: TableListAdapter>(_ dataSources: [Source]) {
-        self.init(SourceBase(dataSources))
-    }
-    
-    init(@AnyTableSourcesBuilder content: () -> Self) {
-        let dataSource = content()
-        self.init(coordinatorSetups: dataSource.coordinatorSetups, source: dataSource.sourceBase)
+    init<Source: TableListAdapter>(@AnyTableSourcesBuilder content: () -> Source) {
+        self.init(content())
     }
 }
 
 @_functionBuilder
 public struct AnyTableSourcesBuilder {
-    public static func buildIf<S: TableListAdapter>(_ content: S?) -> AnyTableSources {
-        AnyTableSources(content.map { [AnyTableSources($0)] } ?? [])
+    public static func buildIf<S: TableListAdapter>(_ content: S?) -> S? {
+        content
     }
 
-    public static func buildEither<TrueContent: TableListAdapter>(first: TrueContent) -> AnyTableSources {
-        AnyTableSources([first])
+    public static func buildEither<TrueContent: TableListAdapter>(first: TrueContent) -> TrueContent {
+        first
     }
 
-    public static func buildEither<FalseContent: TableListAdapter>(second: FalseContent) -> AnyTableSources {
-        AnyTableSources([second])
+    public static func buildEither<FalseContent: TableListAdapter>(second: FalseContent) -> FalseContent {
+        second
     }
     
-    public static func buildBlock() -> AnyTableSources {
-        AnyTableSources([AnyTableSources]())
+    public static func buildBlock() -> TableList<Sources<[AnyTableSources], Any>> {
+        TableList(Sources(dataSources: []))
     }
 
-    public static func buildBlock<S: TableListAdapter>(_ content: S) -> AnyTableSources {
-        AnyTableSources([AnyTableSources(content)])
+    public static func buildBlock<S: TableListAdapter>(_ content: S) -> S {
+        content
     }
     
-    public static func buildBlock<S0: TableListAdapter, S1: TableListAdapter>(_ s0: S0, _ s1: S1) -> AnyTableSources {
-        AnyTableSources([AnyTableSources(s0), AnyTableSources(s1)])
+    public static func buildBlock<S0: TableListAdapter, S1: TableListAdapter>(_ s0: S0, _ s1: S1) -> TableList<Sources<[AnyTableSources], Any>> {
+        TableList(Sources(dataSources: [AnyTableSources(s0), AnyTableSources(s1)]))
     }
     
-    public static func buildBlock<S0: TableListAdapter, S1: TableListAdapter, S2: TableListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2) -> AnyTableSources {
-        AnyTableSources([AnyTableSources(s0), AnyTableSources(s1), AnyTableSources(s2)])
+    public static func buildBlock<S0: TableListAdapter, S1: TableListAdapter, S2: TableListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2) -> TableList<Sources<[AnyTableSources], Any>> {
+        TableList(Sources(dataSources: [AnyTableSources(s0), AnyTableSources(s1), AnyTableSources(s2)]))
     }
     
-    public static func buildBlock<S0: TableListAdapter, S1: TableListAdapter, S2: TableListAdapter, S3: TableListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3) -> AnyTableSources {
-        AnyTableSources([AnyTableSources(s0), AnyTableSources(s1), AnyTableSources(s2), AnyTableSources(s3)])
+    public static func buildBlock<S0: TableListAdapter, S1: TableListAdapter, S2: TableListAdapter, S3: TableListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3) -> TableList<Sources<[AnyTableSources], Any>> {
+        TableList(Sources(dataSources: [AnyTableSources(s0), AnyTableSources(s1), AnyTableSources(s2), AnyTableSources(s3)]))
     }
     
-    public static func buildBlock<S0: TableListAdapter, S1: TableListAdapter, S2: TableListAdapter, S3: TableListAdapter, S4: TableListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4) -> AnyTableSources {
-        AnyTableSources([AnyTableSources(s0), AnyTableSources(s1), AnyTableSources(s2), AnyTableSources(s3), AnyTableSources(s4)])
+    public static func buildBlock<S0: TableListAdapter, S1: TableListAdapter, S2: TableListAdapter, S3: TableListAdapter, S4: TableListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4) -> TableList<Sources<[AnyTableSources], Any>> {
+        TableList(Sources(dataSources: [AnyTableSources(s0), AnyTableSources(s1), AnyTableSources(s2), AnyTableSources(s3), AnyTableSources(s4)]))
     }
     
-    public static func buildBlock<S0: TableListAdapter, S1: TableListAdapter, S2: TableListAdapter, S3: TableListAdapter, S4: TableListAdapter, S5: TableListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5) -> AnyTableSources {
-        AnyTableSources([AnyTableSources(s0), AnyTableSources(s1), AnyTableSources(s2), AnyTableSources(s3), AnyTableSources(s4), AnyTableSources(s5)])
+    public static func buildBlock<S0: TableListAdapter, S1: TableListAdapter, S2: TableListAdapter, S3: TableListAdapter, S4: TableListAdapter, S5: TableListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5) -> TableList<Sources<[AnyTableSources], Any>> {
+        TableList(Sources(dataSources: [AnyTableSources(s0), AnyTableSources(s1), AnyTableSources(s2), AnyTableSources(s3), AnyTableSources(s4), AnyTableSources(s5)]))
     }
     
-    public static func buildBlock<S0: TableListAdapter, S1: TableListAdapter, S2: TableListAdapter, S3: TableListAdapter, S4: TableListAdapter, S5: TableListAdapter, S6: TableListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6) -> AnyTableSources {
-        AnyTableSources([AnyTableSources(s0), AnyTableSources(s1), AnyTableSources(s2), AnyTableSources(s3), AnyTableSources(s4), AnyTableSources(s5), AnyTableSources(s6)])
+    public static func buildBlock<S0: TableListAdapter, S1: TableListAdapter, S2: TableListAdapter, S3: TableListAdapter, S4: TableListAdapter, S5: TableListAdapter, S6: TableListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6) -> TableList<Sources<[AnyTableSources], Any>> {
+        TableList(Sources(dataSources: [AnyTableSources(s0), AnyTableSources(s1), AnyTableSources(s2), AnyTableSources(s3), AnyTableSources(s4), AnyTableSources(s5), AnyTableSources(s6)]))
     }
     
-    public static func buildBlock<S0: TableListAdapter, S1: TableListAdapter, S2: TableListAdapter, S3: TableListAdapter, S4: TableListAdapter, S5: TableListAdapter, S6: TableListAdapter, S7: TableListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6, _ s7: S7) -> AnyTableSources {
-        AnyTableSources([AnyTableSources(s0), AnyTableSources(s1), AnyTableSources(s2), AnyTableSources(s3), AnyTableSources(s4), AnyTableSources(s5), AnyTableSources(s6), AnyTableSources(s7)])
+    public static func buildBlock<S0: TableListAdapter, S1: TableListAdapter, S2: TableListAdapter, S3: TableListAdapter, S4: TableListAdapter, S5: TableListAdapter, S6: TableListAdapter, S7: TableListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6, _ s7: S7) -> TableList<Sources<[AnyTableSources], Any>> {
+        TableList(Sources(dataSources: [AnyTableSources(s0), AnyTableSources(s1), AnyTableSources(s2), AnyTableSources(s3), AnyTableSources(s4), AnyTableSources(s5), AnyTableSources(s6), AnyTableSources(s7)]))
     }
     
-    public static func buildBlock<S0: TableListAdapter, S1: TableListAdapter, S2: TableListAdapter, S3: TableListAdapter, S4: TableListAdapter, S5: TableListAdapter, S6: TableListAdapter, S7: TableListAdapter, S8: TableListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6, _ s7: S7, _ s8: S8) -> AnyTableSources {
-        AnyTableSources([AnyTableSources(s0), AnyTableSources(s1), AnyTableSources(s2), AnyTableSources(s3), AnyTableSources(s4), AnyTableSources(s5), AnyTableSources(s6), AnyTableSources(s7)])
+    public static func buildBlock<S0: TableListAdapter, S1: TableListAdapter, S2: TableListAdapter, S3: TableListAdapter, S4: TableListAdapter, S5: TableListAdapter, S6: TableListAdapter, S7: TableListAdapter, S8: TableListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6, _ s7: S7, _ s8: S8) -> TableList<Sources<[AnyTableSources], Any>> {
+        TableList(Sources(dataSources: [AnyTableSources(s0), AnyTableSources(s1), AnyTableSources(s2), AnyTableSources(s3), AnyTableSources(s4), AnyTableSources(s5), AnyTableSources(s6), AnyTableSources(s7), AnyTableSources(s8)]))
     }
     
-    public static func buildBlock<S0: TableListAdapter, S1: TableListAdapter, S2: TableListAdapter, S3: TableListAdapter, S4: TableListAdapter, S5: TableListAdapter, S6: TableListAdapter, S7: TableListAdapter, S8: TableListAdapter, S9: TableListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6, _ s7: S7, _ s8: S8, _ s9: S9) -> AnyTableSources {
-        AnyTableSources([AnyTableSources(s8), AnyTableSources(s0), AnyTableSources(s1), AnyTableSources(s2), AnyTableSources(s3), AnyTableSources(s4), AnyTableSources(s5), AnyTableSources(s6), AnyTableSources(s7), AnyTableSources(s8), AnyTableSources(s9)])
+    public static func buildBlock<S0: TableListAdapter, S1: TableListAdapter, S2: TableListAdapter, S3: TableListAdapter, S4: TableListAdapter, S5: TableListAdapter, S6: TableListAdapter, S7: TableListAdapter, S8: TableListAdapter, S9: TableListAdapter>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6, _ s7: S7, _ s8: S8, _ s9: S9) -> TableList<Sources<[AnyTableSources], Any>> {
+        TableList(Sources(dataSources: [AnyTableSources(s0), AnyTableSources(s1), AnyTableSources(s2), AnyTableSources(s3), AnyTableSources(s4), AnyTableSources(s5), AnyTableSources(s6), AnyTableSources(s7), AnyTableSources(s8), AnyTableSources(s9)]))
     }
 }

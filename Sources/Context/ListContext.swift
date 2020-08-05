2d95a53dcc32c8644eb6ffde6cebcf5cc0282661
Frain <frainl@outlook.com>
2020-08-05 16:33:31 +0800

    chore: fix not using root to config UI

--- Sources/Context/ListContext.swift
+++ Sources/Context/ListContext.swift
@@ -52,13 +52,13 @@ public extension ListIndexContext where Index == IndexPath {
 extension ListIndexContext where Index == IndexPath {
     var itemValue: SourceBase.Item { context.coordinator.item(at: section, item) }
     func setNestedCache(update: @escaping (Any) -> Void) {
-        context.itemNestedCache[index.section][index.item] = update
+        root.itemNestedCache[index.section][index.item] = update
     }
     
     func itemCache<Cache>(or getter: (Self, SourceBase.Item) -> Cache) -> Cache {
-        context.itemCaches[index.section][index.item] as? Cache ?? {
+        root.itemCaches[index.section][index.item] as? Cache ?? {
             let cache = getter(self, itemValue)
-            context.itemCaches[index.section][index.item] = cache
+            root.itemCaches[index.section][index.item] = cache
             return cache
         }()
     }

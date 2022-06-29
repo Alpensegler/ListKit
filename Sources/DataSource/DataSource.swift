39817398cde77b7f821847f03eaec9f5e0ae78fc
Frain <frainl@outlook.com>
2022-06-29 21:05:21 +0800

    refactor: new DataSource API and Builder

--- Sources/DataSource/DataSource.swift
+++ Sources/DataSource/DataSource.swift
@@ -1,20 +1,33 @@
 //
-//  Source.swift
+//  DataSource.swift
 //  ListKit
 //
 //  Created by Frain on 2019/4/8.
 //
 
-public protocol DataSource {
+/// A type representing the datasource of a list
+public protocol DataSource<Source, Item> {
+    /// A type representing the model of a single cell
     associatedtype Item
+    
+    /// The type of datasource representing the content of the list
     associatedtype Source = [Item]
-    associatedtype SourceBase: DataSource = Self
-        where SourceBase.Item == Item, SourceBase.SourceBase == SourceBase
+    
+    /// A replacment of `Self`
+    associatedtype Base: DataSource = Self
+    where Base.Item == Item, Base.Base == Base
+    
+    /// The content and behavior of the list
+    @DataSourceBuilder<Base>
+    var source: Source { get }
+    
+    /// The base of the datasource, euqal to `Self` in normal cases
+    var base: Base { get }
+    
+    #if !Next
     associatedtype AdapterBase: DataSource = Self
         where AdapterBase.Item == Item, AdapterBase.AdapterBase == AdapterBase
     
-    var source: Source { get }
-    var sourceBase: SourceBase { get }
     var adapterBase: AdapterBase { get }
     
     var listOptions: ListOptions { get }
@@ -24,8 +37,14 @@ public protocol DataSource {
     var listDelegate: ListDelegate { get }
     var listCoordinator: ListCoordinator<SourceBase> { get }
     var listCoordinatorContext: ListCoordinatorContext<SourceBase> { get }
+    #endif
 }
 
+public extension DataSource where Base == Self {
+    var base: Base { self }
+}
+
+#if !Next
 public extension DataSource {
     var listDelegate: ListDelegate { .init() }
     var listCoordinator: ListCoordinator<SourceBase> {
@@ -36,10 +55,7 @@ public extension DataSource {
     }
 }
 
-public extension DataSource where SourceBase == Self {
-    var sourceBase: SourceBase { self }
-}
-
 public extension DataSource where AdapterBase == Self {
     var adapterBase: AdapterBase { self }
 }
+#endif
